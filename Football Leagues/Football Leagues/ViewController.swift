16e555496c671b4e369a500757188803031dbd4d
Ayman Fathy <aymanfathy367@gmail.com>
2019-06-30 13:44:26 +0200

    setup project

---
+++ Football Leagues/Football Leagues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Football Leagues
+//
+//  Created by Ayman Fathy on 6/30/19.
+//  Copyright © 2019 Technivance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
